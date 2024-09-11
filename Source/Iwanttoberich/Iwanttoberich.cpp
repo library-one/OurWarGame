e0dc3eae3c1c3402b4fe83a39288b189ca3bcf17
library-one <118429457+library-one@users.noreply.github.com>
2024-09-11 13:23:07 +0900

    Delete Source directory

--- Source/Iwanttoberich/Iwanttoberich.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Iwanttoberich.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Iwanttoberich, "Iwanttoberich" );
- 
